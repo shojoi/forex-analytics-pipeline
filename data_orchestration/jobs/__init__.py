52294f2094e9b4b279bfba1f1454c4103a4611e9
shojoi <shobha.code@outlook.com>
2026-05-03 19:41:22 +1000

    Comment out Airbyte assets import for dbt-only mode

--- data_orchestration/jobs/__init__.py
+++ data_orchestration/jobs/__init__.py
@@ -1,8 +1,8 @@
 
 from data_orchestration.jobs.forex_pipeline_job import (
-    forex_pipeline_job,
+    #forex_pipeline_job,
     dbt_only_job,
-    airbyte_only_job,
+    #airbyte_only_job,
     freshness_only_job
 )
 
