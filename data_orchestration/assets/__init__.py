5bb69a1c8338564b08de4c056ce56d4e5773f265
shojoi <shobha.code@outlook.com>
2026-05-03 19:25:35 +1000

    Comment out Airbyte assets import for dbt-only mode

--- data_orchestration/assets/__init__.py
+++ data_orchestration/assets/__init__.py
@@ -1,5 +1,5 @@
 
-from data_orchestration.assets.airbyte_assets import airbyte_assets
+#from data_orchestration.assets.airbyte_assets import airbyte_assets
 from data_orchestration.assets.dbt_assets import forex_dbt_assets
 from data_orchestration.assets.data_freshness_asset import data_freshness_tracker
 
